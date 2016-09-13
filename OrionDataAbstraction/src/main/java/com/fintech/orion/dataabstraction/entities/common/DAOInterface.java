fc63ae253ad646380bd65954d4e08a2c6ba0269e
Sasitha Gunadasa <sasithag@zone24x7.com>
2016-09-13 10:16:17 +0530

    updated build files added Entity classes

---
+++ OrionDataAbstraction/src/main/java/com/fintech/orion/dataabstraction/entities/common/DAOInterface.java
@@ -0,0 +1,11 @@
+package com.fintech.orion.dataabstraction.entities.common;
+
+import java.io.Serializable;
+
+/**
+ *
+ * Created by sasitha on 9/12/16.
+ */
+public interface DAOInterface<E, I extends Serializable> {
+
+}
