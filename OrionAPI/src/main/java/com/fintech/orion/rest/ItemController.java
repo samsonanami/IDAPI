a23e45d786839cae4e8f3743fe80bcd291b58e52
chathurangarw <chathurangarw@zone24x7.com>
2016-10-07 11:50:49 +0530

    Controller endpoints

---
+++ OrionAPI/src/main/java/com/fintech/orion/rest/ItemController.java
@@ -0,0 +1,124 @@
+package com.fintech.orion.rest;
+
+import com.fintech.orion.coreservices.ResourceServiceInterface;
+import com.fintech.orion.dataabstraction.entities.orion.Resource;
+import com.fintech.orion.dataabstraction.exceptions.ItemNotFoundException;
+import com.fintech.orion.dataabstraction.models.ProcessingRequest;
+import com.fintech.orion.helper.*;
+import com.fintech.orion.dataabstraction.helper.GenerateUUID;
+import com.fintech.orion.model.ContentUploadResourceResult;
+import com.fintech.orion.model.VerificationResponseMessage;
+import org.apache.log4j.Logger;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.*;
+import org.springframework.web.multipart.MultipartFile;
+
+import javax.servlet.http.HttpServletResponse;
+
+@Controller
+public class ItemController {
+    private static final String THE_SUBMITTED_FILE_IS_NOT_IN_CORRECT_FORMAT = "The submitted file is not in correct format";
+    private static final String FILE_UPLOAD_ERROR = "File Upload Error";
+    private static final String JSON_NOT_IN_CORRECT_FORMAT = "Json not in correct format";
+
+    private static final Logger LOGGER = Logger.getLogger(ItemController.class);
+
+    @Autowired
+    private ResourceServiceInterface resourceServiceInterface;
+
+    @Autowired
+    private FileExtensionValidatorInterface fileExtensionValidatorInterface;
+
+    @Autowired
+    private FileUploadHandlerInterface fileUploadHandlerInterface;
+
+    @Autowired
+    private JsonValidatorInterface jsonValidatorInterface;
+
+    @Autowired
+    private ProcessingRequestHandlerInterface processingRequestHandlerInterface;
+
+    @RequestMapping(value = "v1/content/{contentType}", method = RequestMethod.POST)
+    @ResponseBody
+    public Object uploadContent(@PathVariable String contentType,
+                                HttpServletResponse response,
+                                @RequestParam("access_token") String accessToken,
+                                @RequestParam("file") final MultipartFile multiPart) {
+        try {
+            Resource resource;
+            String fileName = multiPart.getOriginalFilename();
+            String extension = fileName.split("\\.")[1];
+            String uuidNumber = GenerateUUID.uuidNumber();
+            String newFilename = uuidNumber + "." + extension;
+            if (!fileExtensionValidatorInterface.validate(extension)) {
+                return ErrorHandler.renderError(HttpServletResponse.SC_BAD_REQUEST, THE_SUBMITTED_FILE_IS_NOT_IN_CORRECT_FORMAT, response);
+            }
+
+            boolean isSaved = fileUploadHandlerInterface.upload(multiPart, newFilename);
+
+            if(isSaved) {
+                resource = resourceServiceInterface.saveResource(newFilename, uuidNumber, contentType, accessToken);
+            } else {
+                return ErrorHandler.renderError(HttpServletResponse.SC_INTERNAL_SERVER_ERROR, FILE_UPLOAD_ERROR, response);
+            }
+            ContentUploadResourceResult result = new ContentUploadResourceResult();
+            result.setResourceReferenceCode(resource.getResourceIdentificationCode());
+            return result;
+        } catch (ItemNotFoundException ex) {
+            LOGGER.error(ex);
+            LOGGER.error(ex.getMessage());
+            return ErrorHandler.renderError(HttpServletResponse.SC_BAD_REQUEST, ex.getMessage(), response);
+        }
+    }
+
+    @RequestMapping(value = "v1/verification", method = RequestMethod.POST)
+    @ResponseBody
+    public Object verification(@RequestParam(name = "integration_request", required = false) boolean integrationRequest,
+                               HttpServletResponse response,
+                               @RequestParam("access_token") String accessToken,
+                               @RequestBody ProcessingRequest data) {
+        try {
+            if(!jsonValidatorInterface.jsonValidate(data.getVerificationProcesses())){
+                return ErrorHandler.renderError(HttpServletResponse.SC_BAD_REQUEST, JSON_NOT_IN_CORRECT_FORMAT, response);
+            }
+
+            String processingRequestId = processingRequestHandlerInterface.saveData(accessToken, data.getVerificationProcesses());
+
+            VerificationResponseMessage result = new VerificationResponseMessage();
+            result.setProcessingRequestId(processingRequestId);
+            return result;
+        } catch (Exception ex){
+            LOGGER.error(ex);
+            LOGGER.error(ex.getMessage());
+            return ErrorHandler.renderError(HttpServletResponse.SC_BAD_REQUEST, ex.getMessage(), response);
+        }
+    }
+
+    @RequestMapping(value = "v1/verification/{verificationRequestId}", method = RequestMethod.GET)
+    @ResponseBody
+    public Object verificationResults(@PathVariable String verificationRequestId,
+                                HttpServletResponse response,
+                                @RequestParam("access_token") String accessToken) {
+        try {
+            // TODO
+            return null;
+        } catch (Exception ex){
+            throw ex;
+        }
+    }
+
+    @RequestMapping(value = "v1/verification/{verificationProcessId}/resource/{id}", method = RequestMethod.GET)
+    @ResponseBody
+    public Object processedResources(@PathVariable String verificationProcessId,
+                               @PathVariable int id,
+                               HttpServletResponse response,
+                               @RequestParam("access_token") String accessToken) {
+        try {
+            // TODO
+            return null;
+        } catch (Exception ex){
+            throw ex;
+        }
+    }
+}
---
+++ OrionAPI/src/test/java/com/fintech/orion/ItemControllerTest.java
@@ -0,0 +1,47 @@
+package com.fintech.orion;
+
+import com.fintech.orion.coreservices.ResourceService;
+import com.fintech.orion.coreservices.ResourceServiceInterface;
+import com.fintech.orion.dataabstraction.exceptions.ItemNotFoundException;
+import com.fintech.orion.helper.*;
+import com.fintech.orion.model.ContentUploadResourceResult;
+import com.fintech.orion.rest.ItemController;
+import org.junit.Test;
+import org.springframework.mock.web.MockHttpServletResponse;
+import org.springframework.mock.web.MockMultipartFile;
+import org.springframework.test.util.ReflectionTestUtils;
+
+import static org.hamcrest.core.IsInstanceOf.instanceOf;
+import static org.junit.Assert.assertThat;
+import static org.mockito.Mockito.mock;
+import static org.mockito.Mockito.when;
+
+public class ItemControllerTest {
+
+    @Test
+    public void shouldReturnResourceReferenceCodeWhenUploadFileCalled() throws ItemNotFoundException {
+        ItemController itemController = new ItemController();
+
+        final String filename = "image.jpg";
+        final byte[] content = "image data".getBytes();
+        MockMultipartFile mockMultipartFile = new MockMultipartFile("content", filename, "text/plain", content);
+        MockHttpServletResponse response = new MockHttpServletResponse();
+        String assessToken = "123456";
+        String contentType = "image";
+
+        FileExtensionValidatorInterface fileExtensionValidatorInterfaceMock = mock(FileExtensionValidator.class);
+        when(fileExtensionValidatorInterfaceMock.validate("jpg")).thenReturn(true);
+        ReflectionTestUtils.setField(itemController, "fileExtensionValidatorInterface", fileExtensionValidatorInterfaceMock);
+
+        FileUploadHandlerInterface fileUploadHandlerInterfaceMock = mock(FileUploadHandler.class);
+        when(fileUploadHandlerInterfaceMock.upload(mockMultipartFile, filename)).thenReturn(true);
+        ReflectionTestUtils.setField(itemController, "fileSaveHandlerInterface", fileUploadHandlerInterfaceMock);
+
+        ResourceServiceInterface resourceServiceInterfaceMock = mock(ResourceService.class);
+        ReflectionTestUtils.setField(itemController, "resourceServiceInterface", resourceServiceInterfaceMock);
+
+        Object resourceReferenceCode = itemController.uploadContent(contentType, response, assessToken, mockMultipartFile);
+        assertThat(resourceReferenceCode, instanceOf(ContentUploadResourceResult.class));
+    }
+
+}
