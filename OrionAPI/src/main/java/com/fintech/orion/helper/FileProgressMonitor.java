f6b9364e64314ebc7ee15e08175dd4a7597231cd
chathurangarw <chathurangarw@zone24x7.com>
2016-10-07 11:25:07 +0530

    File upload handler classes

---
+++ OrionAPI/src/main/java/com/fintech/orion/helper/FileProgressMonitor.java
@@ -0,0 +1,44 @@
+package com.fintech.orion.helper;
+
+import com.jcraft.jsch.SftpProgressMonitor;
+import org.apache.log4j.Logger;
+
+import javax.swing.*;
+
+public class FileProgressMonitor implements SftpProgressMonitor {
+    ProgressMonitor monitor;
+    long count = 0;
+    long max = 0;
+    private long percent = 1000;
+
+    private static final Logger LOGGER = Logger.getLogger(FileProgressMonitor.class);
+
+    @Override
+    public void init(int op, String src, String dest, long max) {
+        this.max = max;
+        monitor = new ProgressMonitor(null,
+                ((op == SftpProgressMonitor.PUT) ?
+                        "put" : "get") + ": " + src,
+                "", 0, (int) max);
+        count = 0;
+        percent = -1;
+        monitor.setProgress((int) this.count);
+        monitor.setMillisToDecideToPopup(1000);
+    }
+
+    @Override
+    public boolean count(long count) {
+        this.count += count;
+        percent = this.count * 100 / max;
+        LOGGER.info("Completed " + this.count + " byte(s) (" + percent + "%) out of " + max + " byte(s).");
+        monitor.setNote("Completed " + this.count + " byte(s) (" + percent + "%) out of " + max + " byte(s).");
+        monitor.setProgress((int) this.count);
+
+        return !(monitor.isCanceled());
+    }
+
+    @Override
+    public void end() {
+        monitor.close();
+    }
+}
---
+++ OrionAPI/src/main/java/com/fintech/orion/helper/FileUploadHandler.java
@@ -0,0 +1,53 @@
+package com.fintech.orion.helper;
+
+import com.fintech.orion.model.Configuration;
+import org.apache.log4j.Logger;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.multipart.MultipartFile;
+
+import java.io.*;
+
+public class FileUploadHandler implements FileUploadHandlerInterface {
+
+    private static final Logger LOGGER = Logger.getLogger(SFileTransfer.class);
+
+    @Autowired
+    private Configuration configuration;
+
+    @Autowired
+    private String workingDir;
+
+    @Autowired
+    private FileSizeCheckerInterface fileSizeCheckerInterface;
+
+    @Override
+    public boolean upload(MultipartFile multipartFile, String filename) {
+        try {
+            File file = new File(filename);
+            multipartFile.transferTo(file);
+
+            if(fileSizeCheckerInterface.checkSize(multipartFile)) {
+
+                SFileTransfer sFileTransfer = new SFileTransfer(configuration);
+
+                LOGGER.info("Uploading File.");
+
+                if (sFileTransfer.transferFile(file.getPath(), workingDir)) {
+                    LOGGER.info("Uploading File Complete.");
+                    return true;
+                } else {
+                    LOGGER.info("Uploading File Failed.");
+                    return false;
+                }
+            } else {
+                LOGGER.info("File Size Not Allowed.");
+                return false;
+            }
+        } catch (IOException e) {
+            LOGGER.error(e);
+            LOGGER.error("File Not Found." + e.getMessage());
+            return false;
+        }
+    }
+
+}
