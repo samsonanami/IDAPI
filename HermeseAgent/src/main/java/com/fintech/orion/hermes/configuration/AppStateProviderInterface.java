c8ed3d28614bd2867b2042ab0949e32fd9ff2e61
Tharindu <tharindump@zone24x7.com>
2016-10-10 17:50:51 +0530

    IDAPI : Hermes : Initial start with Application Service and context setting

---
+++ HermeseAgent/src/main/java/com/fintech/orion/hermes/configuration/AppStateProviderInterface.java
@@ -0,0 +1,8 @@
+package com.fintech.orion.hermes.configuration;
+
+/**
+ * Created by TharinduMP on 10/10/2016.
+ */
+public interface AppStateProviderInterface {
+    boolean isAgentOnDebugState();
+}
---
+++ HermeseAgent/src/main/java/com/fintech/orion/hermes/listener/HermesListener.java
@@ -0,0 +1,14 @@
+package com.fintech.orion.hermes.listener;
+
+import javax.jms.Message;
+import javax.jms.MessageListener;
+
+/**
+ * Created by TharinduMP on 10/10/2016.
+ */
+public class HermesListener implements MessageListener {
+    @Override
+    public void onMessage(Message message) {
+
+    }
+}
