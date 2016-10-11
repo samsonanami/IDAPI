1dec3156a72b227dfd5039207a2dbc69b4a6fcae
chathurangarw <chathurangarw@zone24x7.com>
2016-10-11 07:04:21 +0530

    Generic service methods

--- Services/CoreServices/src/main/java/com/fintech/orion/coreservices/ProcessTypeLicenseService.java
+++ Services/CoreServices/src/main/java/com/fintech/orion/coreservices/ProcessTypeLicenseService.java
@@ -1,55 +1,10 @@
 package com.fintech.orion.coreservices;
 
+import com.fintech.orion.common.AbstractService;
 import com.fintech.orion.dataabstraction.entities.orion.ProcessTypeLicense;
-import com.fintech.orion.dataabstraction.exceptions.ItemNotFoundException;
-import com.fintech.orion.dataabstraction.repositories.ProcessTypeLicenseRepositoryInterface;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
-import org.springframework.transaction.annotation.Transactional;
-
-import java.util.List;
 
 @Service
-public class ProcessTypeLicenseService implements ProcessTypeLicenseServiceInterface {
+public class ProcessTypeLicenseService extends AbstractService<ProcessTypeLicense, Integer> implements ProcessTypeLicenseServiceInterface {
 
-    @Autowired
-    private ProcessTypeLicenseRepositoryInterface processTypeLicenseRepositoryInterface;
-
-    @Transactional
-    @Override
-    public List<ProcessTypeLicense> getProcessTypeLicenseList() {
-        return processTypeLicenseRepositoryInterface.getAll();
-    }
-
-    @Transactional
-    @Override
-    public ProcessTypeLicense getProcessTypeLicenseById(int id) throws ItemNotFoundException {
-        ProcessTypeLicense processTypeLicense = processTypeLicenseRepositoryInterface.findById(id);
-        if (processTypeLicense != null) {
-            return processTypeLicense;
-        } else { throw new ItemNotFoundException("Item Not Found"); }
-    }
-
-    @Transactional
-    @Override
-    public void saveOrUpdateProcessTypeLicense(ProcessTypeLicense processTypeLicense) {
-        processTypeLicenseRepositoryInterface.saveOrUpdate(processTypeLicense);
-    }
-
-    @Transactional
-    @Override
-    public boolean deleteProcessTypeLicenseById(int id) throws ItemNotFoundException {
-        ProcessTypeLicense processTypeLicense = processTypeLicenseRepositoryInterface.findById(id);
-        if(processTypeLicense != null){
-            processTypeLicenseRepositoryInterface.delete(processTypeLicense);
-            return true;
-        }
-        return false;
-    }
-
-    @Transactional
-    @Override
-    public void deleteProcessTypeLicense(ProcessTypeLicense processTypeLicense) {
-        processTypeLicenseRepositoryInterface.delete(processTypeLicense);
-    }
 }
