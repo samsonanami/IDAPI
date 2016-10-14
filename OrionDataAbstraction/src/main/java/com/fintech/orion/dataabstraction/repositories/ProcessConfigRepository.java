b61dd8a8e672b58f37bbe41abf36b48f0b28be34
chathurangarw <chathurangarw@zone24x7.com>
2016-10-14 11:09:38 +0530

    Process Config entity changes

--- OrionDataAbstraction/src/main/java/com/fintech/orion/dataabstraction/repositories/ProcessConfigRepository.java
+++ OrionDataAbstraction/src/main/java/com/fintech/orion/dataabstraction/repositories/ProcessConfigRepository.java
@@ -7,8 +7,8 @@ import org.springframework.stereotype.Repository;
 @Repository
 public class ProcessConfigRepository extends AbstractDAO<ProcessConfig, Integer> implements ProcessConfigRepositoryInterface {
 
-    protected ProcessConfigRepository(Class<ProcessConfig> entityClass) {
-        super(entityClass);
+    protected ProcessConfigRepository() {
+        super(ProcessConfig.class);
     }
 
 }
