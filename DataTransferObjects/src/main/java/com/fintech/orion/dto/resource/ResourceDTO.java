cf6ccc94a78e30b672c9ca95302fc59f391a29a5
chathurangarw <chathurangarw@zone24x7.com>
2016-10-17 10:54:47 +0530

    DTO classes for entities

---
+++ DataTransferObjects/src/main/java/com/fintech/orion/dto/resource/ResourceDTO.java
@@ -0,0 +1,69 @@
+package com.fintech.orion.dto.resource;
+
+import com.fintech.orion.dataabstraction.entities.orion.Client;
+import com.fintech.orion.dataabstraction.entities.orion.Process;
+import com.fintech.orion.dataabstraction.entities.orion.ResourceType;
+import com.fintech.orion.dto.client.ClientDTO;
+import com.fintech.orion.dto.process.ProcessDTO;
+import com.fintech.orion.dto.resourcetype.ResourceTypeDTO;
+
+/**
+ * Created by ChathurangaRW on 10/17/2016.
+ */
+public class ResourceDTO {
+
+    private Integer id;
+    private ClientDTO clientDTO;
+    private ProcessDTO processDTO;
+    private ResourceTypeDTO resourceTypeDTO;
+    private String location;
+    private String resourceIdentificationCode;
+
+    public Integer getId() {
+        return id;
+    }
+
+    public void setId(Integer id) {
+        this.id = id;
+    }
+
+    public ClientDTO getClientDTO() {
+        return clientDTO;
+    }
+
+    public void setClientDTO(ClientDTO clientDTO) {
+        this.clientDTO = clientDTO;
+    }
+
+    public ProcessDTO getProcessDTO() {
+        return processDTO;
+    }
+
+    public void setProcessDTO(ProcessDTO processDTO) {
+        this.processDTO = processDTO;
+    }
+
+    public ResourceTypeDTO getResourceTypeDTO() {
+        return resourceTypeDTO;
+    }
+
+    public void setResourceTypeDTO(ResourceTypeDTO resourceTypeDTO) {
+        this.resourceTypeDTO = resourceTypeDTO;
+    }
+
+    public String getLocation() {
+        return location;
+    }
+
+    public void setLocation(String location) {
+        this.location = location;
+    }
+
+    public String getResourceIdentificationCode() {
+        return resourceIdentificationCode;
+    }
+
+    public void setResourceIdentificationCode(String resourceIdentificationCode) {
+        this.resourceIdentificationCode = resourceIdentificationCode;
+    }
+}
---
+++ DataTransferObjects/src/main/java/com/fintech/orion/dto/resourcetype/ResourceTypeDTO.java
@@ -0,0 +1,40 @@
+package com.fintech.orion.dto.resourcetype;
+
+import com.fintech.orion.dto.resource.ResourceDTO;
+
+import java.util.HashSet;
+import java.util.Set;
+
+/**
+ * Created by ChathurangaRW on 10/17/2016.
+ */
+public class ResourceTypeDTO {
+
+    private int id;
+    private String type;
+    private Set<ResourceDTO> resources = new HashSet<ResourceDTO>(0);
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getType() {
+        return type;
+    }
+
+    public void setType(String type) {
+        this.type = type;
+    }
+
+    public Set<ResourceDTO> getResources() {
+        return resources;
+    }
+
+    public void setResources(Set<ResourceDTO> resources) {
+        this.resources = resources;
+    }
+}
