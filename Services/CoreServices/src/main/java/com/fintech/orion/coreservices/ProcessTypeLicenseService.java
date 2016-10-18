c709cc33e72d78c3294945cc8c1c79f5e7b07d37
chathurangarw <chathurangarw@zone24x7.com>
2016-10-18 14:43:46 +0530

    ProcessTypeLicense service modification with dtos

--- Services/CoreServices/src/main/java/com/fintech/orion/coreservices/ProcessTypeLicenseService.java
+++ Services/CoreServices/src/main/java/com/fintech/orion/coreservices/ProcessTypeLicenseService.java
@@ -2,12 +2,41 @@ package com.fintech.orion.coreservices;
 
 import com.fintech.orion.common.AbstractService;
 import com.fintech.orion.dataabstraction.entities.orion.ProcessTypeLicense;
+import com.fintech.orion.dataabstraction.exceptions.ItemNotFoundException;
+import com.fintech.orion.dto.processtypelicense.ProcessTypeLicenseDTO;
+import com.fintech.orion.mapping.processtypelicense.ProcessTypeLicenseMapper;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+
+import java.util.List;
 
 /**
  * ProcessTypeLicense entity service class
  */
 @Service
 public class ProcessTypeLicenseService extends AbstractService<ProcessTypeLicense, Integer> implements ProcessTypeLicenseServiceInterface {
+
+    @Autowired
+    private ProcessTypeLicenseMapper processTypeLicenseMapper;
+
+    @Override
+    public List<ProcessTypeLicenseDTO> getAllDTOs() {
+        return processTypeLicenseMapper.processTypeLicensesToProcessTypeLicenseDTOs(getAll());
+    }
+
+    @Override
+    public ProcessTypeLicenseDTO findById(int id) throws ItemNotFoundException {
+        return processTypeLicenseMapper.processTypeLicenseToProcessTypeLicenseDTO(findById(new Integer(id)));
+    }
+
+    @Override
+    public void saveOrUpdate(ProcessTypeLicenseDTO processTypeLicenseDTO) {
+        saveOrUpdate(processTypeLicenseMapper.processTypeLicenseDTOToProcessTypeLicense(processTypeLicenseDTO));
+    }
+
+    @Override
+    public void delete(ProcessTypeLicenseDTO processTypeLicenseDTO) {
+        delete(processTypeLicenseMapper.processTypeLicenseDTOToProcessTypeLicense(processTypeLicenseDTO));
+    }
 
 }
--- Services/CoreServices/src/main/java/com/fintech/orion/coreservices/ProcessTypeLicenseServiceInterface.java
+++ Services/CoreServices/src/main/java/com/fintech/orion/coreservices/ProcessTypeLicenseServiceInterface.java
@@ -2,10 +2,22 @@ package com.fintech.orion.coreservices;
 
 import com.fintech.orion.common.ServiceInterface;
 import com.fintech.orion.dataabstraction.entities.orion.ProcessTypeLicense;
+import com.fintech.orion.dataabstraction.exceptions.ItemNotFoundException;
+import com.fintech.orion.dto.processtypelicense.ProcessTypeLicenseDTO;
+
+import java.util.List;
 
 /**
  * ProcessTypeLicense entity service interface
  */
 public interface ProcessTypeLicenseServiceInterface extends ServiceInterface<ProcessTypeLicense, Integer> {
+
+    List<ProcessTypeLicenseDTO> getAllDTOs();
+
+    ProcessTypeLicenseDTO findById(int id) throws ItemNotFoundException;
+
+    void saveOrUpdate(ProcessTypeLicenseDTO processTypeLicenseDTO);
+
+    void delete(ProcessTypeLicenseDTO processTypeLicenseDTO);
 
 }
