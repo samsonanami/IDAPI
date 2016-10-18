51ed0c53569e080e9b6143d8eeea771eb6efa51d
chathurangarw <chathurangarw@zone24x7.com>
2016-10-18 10:06:47 +0530

    Class level commenting

--- Services/CoreServices/src/main/java/com/fintech/orion/coreservices/ClientServiceInterface.java
+++ Services/CoreServices/src/main/java/com/fintech/orion/coreservices/ClientServiceInterface.java
@@ -5,6 +5,9 @@ import com.fintech.orion.dataabstraction.entities.orion.Client;
 import com.fintech.orion.dataabstraction.exceptions.ItemNotFoundException;
 import com.fintech.orion.dto.client.ClientDTO;
 
+/**
+ * Client entity service interface
+ */
 public interface ClientServiceInterface extends ServiceInterface<Client, Integer> {
 
     ClientDTO findByAuthToken(String authToken) throws ItemNotFoundException;
