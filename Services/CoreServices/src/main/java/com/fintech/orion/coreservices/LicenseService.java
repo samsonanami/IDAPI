1c8e0cb2c089ef4487e80f06ea48e2aa0e9f6a6f
chathurangarw <chathurangarw@zone24x7.com>
2016-10-18 13:35:34 +0530

    Client service modification with dtos

--- Services/CoreServices/src/main/java/com/fintech/orion/coreservices/LicenseService.java
+++ Services/CoreServices/src/main/java/com/fintech/orion/coreservices/LicenseService.java
@@ -2,7 +2,13 @@ package com.fintech.orion.coreservices;
 
 import com.fintech.orion.common.AbstractService;
 import com.fintech.orion.dataabstraction.entities.orion.License;
+import com.fintech.orion.dataabstraction.exceptions.ItemNotFoundException;
+import com.fintech.orion.dto.license.LicenseDTO;
+import com.fintech.orion.mapping.license.LicenseMapper;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+
+import java.util.List;
 
 /**
  * License entity service class
@@ -10,4 +16,26 @@ import org.springframework.stereotype.Service;
 @Service
 public class LicenseService extends AbstractService<License, Integer> implements LicenseServiceInterface {
 
+    @Autowired
+    private LicenseMapper licenseMapper;
+
+    @Override
+    public List<LicenseDTO> getAllDTOs() {
+        return licenseMapper.licensesToLicenseDTOs(getAll());
+    }
+
+    @Override
+    public LicenseDTO findById(int id) throws ItemNotFoundException {
+        return licenseMapper.licenseToLicenseDTO(findById(new Integer(id)));
+    }
+
+    @Override
+    public void saveOrUpdate(LicenseDTO licenseDTO) {
+        saveOrUpdate(licenseMapper.licenseDTOToLicense(licenseDTO));
+    }
+
+    @Override
+    public void delete(LicenseDTO licenseDTO) {
+        delete(licenseMapper.licenseDTOToLicense(licenseDTO));
+    }
 }
--- Services/CoreServices/src/main/java/com/fintech/orion/coreservices/LicenseServiceInterface.java
+++ Services/CoreServices/src/main/java/com/fintech/orion/coreservices/LicenseServiceInterface.java
@@ -2,10 +2,22 @@ package com.fintech.orion.coreservices;
 
 import com.fintech.orion.common.ServiceInterface;
 import com.fintech.orion.dataabstraction.entities.orion.License;
+import com.fintech.orion.dataabstraction.exceptions.ItemNotFoundException;
+import com.fintech.orion.dto.license.LicenseDTO;
+
+import java.util.List;
 
 /**
  * License entity service interface
  */
 public interface LicenseServiceInterface extends ServiceInterface<License, Integer> {
+
+    List<LicenseDTO> getAllDTOs();
+
+    LicenseDTO findById(int id) throws ItemNotFoundException;
+
+    void saveOrUpdate(LicenseDTO clientLicenseDTO);
+
+    void delete(LicenseDTO clientLicenseDTO);
 
 }
