23ed94541f233f9713f953548d11c553d9ca8be5
Dinuka Jayamaha <dinukaj@zone24x7.com>
2017-02-06 14:12:31 +0530

    Update war_deployer.sh

--- SCRIPTS/war_deployer.sh
+++ SCRIPTS/war_deployer.sh
@@ -24,3 +24,34 @@ if [ "$2" == "true" ]
 then
 	unzip -l $COPY_LOC/dev/*.zip | sed -n '4,$p' | head -n -2 |awk '{ print $4 }'| grep 'war' | grep 'OrionAuthAPI' | cut -d'-' -f1 >> $WILDFLY_LOC/temp/temp_zipped_war.file
 fi
+
+unzip -l $COPY_LOC/dev/*.zip | sed -n '4,$p' | head -n -2 |awk '{ print $4 }'| grep 'war' | cut -d'-' -f1 > $WILDFLY_LOC/temp/temp_zipped_war.file
+
+echo "################## Files To Be Deployed On Wildfly ######################" 
+cat $WILDFLY_LOC/temp/temp_zipped_war.file
+
+lines=$($WILDFLY_LOC/bin/jboss-cli.sh --connect --commands="ls deployment" | wc -l)
+
+while IFS='' read -r lines1 || [[ -n "$lines1" ]]; do
+	while IFS='' read -r lines2 || [[ -n "$lines2" ]]; do
+		echo "$lines1" | grep "$lines2" >>  $WILDFLY_LOC/temp/temp_undeploy.file
+	done < $WILDFLY_LOC/temp/temp_zipped_war.file
+done < $WILDFLY_LOC/temp/temp_running_war.file
+
+while IFS='' read -r lines3 || [[ -n "$lines3" ]]; do
+	$WILDFLY_LOC/bin/jboss-cli.sh --connect --command="undeploy '$lines3'"
+done < $WILDFLY_LOC/temp/temp_undeploy.file
+
+unzip -l $COPY_LOC/dev/*.zip | sed -n '4,$p' | head -n -2 |awk '{ print $4 }'| grep 'war'> $WILDFLY_LOC/temp/temp_zipped_war.file
+
+while IFS='' read -r lines4 || [[ -n "$lines4" ]]; do
+	$WILDFLY_LOC/bin/jboss-cli.sh --connect --command="deploy --force $WILDFLY_LOC/temp/zip/$lines4"
+    if [ $? -eq 0 ]
+    then
+        echo "Successfully Deployed $lines4"
+    else
+        echo "$lines4 Deployment Unsuccessfull" >&2
+    fi
+done < $WILDFLY_LOC/temp/temp_zipped_war.file
+
+rm -rf  $WILDFLY_LOC/temp/
