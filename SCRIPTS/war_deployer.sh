97d4c6502f90945541438c2d6ed96934ed380d03
Dinuka Jayamaha <dinukaj@zone24x7.com>
2017-02-07 15:22:59 +0530

    Update war_deployer.sh

--- SCRIPTS/war_deployer.sh
+++ SCRIPTS/war_deployer.sh
@@ -162,7 +162,6 @@ hermes_app_deploy()
 
 if [ "$1" == "true" ] || [ "$2" == "true" ]
 then
-    echo $4
     echo "Checking Wether wildfly is running or not"; wildfly_check $4
     #echo "Deploying War Engine"; wildfly_deploy $1 $2
 fi
