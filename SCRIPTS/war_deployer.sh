3848d8fb202e3b42ddd20091a18561bbd25cf95c
Dinuka Jayamaha <dinukaj@zone24x7.com>
2017-02-07 10:27:47 +0530

    Update war_deployer.sh

--- SCRIPTS/war_deployer.sh
+++ SCRIPTS/war_deployer.sh
@@ -121,13 +121,25 @@ hermese_deletebackup()
 	fi
 }
 
-hermese_temp_backup ()
+hermese_temp_backup()
 {
+    rm -rf $HERMESE_APP_LOC/temp
 	mkdir -p $HERMESE_APP_LOC/temp
     cp $HERMESE_APP_LOC/$BUILD_NAME/config/spring-beans.xml $HERMESE_APP_LOC/temp/.
     cp $HERMESE_APP_LOC/$BUILD_NAME/config/spring-config.xml $HERMESE_APP_LOC/temp/.
     cp $HERMESE_APP_LOC/$BUILD_NAME/config/spring-datasource.xml $HERMESE_APP_LOC/temp/.
     cp $HERMESE_APP_LOC/$BUILD_NAME/config/applicationContext.xml $HERMESE_APP_LOC/temp/.
+    cp $HERMESE_APP_LOC/$BUILD_NAME/Hermes.sh $HERMESE_APP_LOC/temp/.
+}
+
+app_deploy()
+{
+	yes | cp -rf $COPY_LOC/dev/*.zip  $HERMESE_APP_LOC/temp/.
+    cd $HERMESE_APP_LOC/temp/
+    rm -rf $HERMESE_APP_LOC/temp/*.war
+    unzip *.zip
+    rm -rf $HERMESE_APP_LOC/$BUILD_NAME
+    $HERMESE_APP_LOC/temp/*.bsx
 }
 
 if [ "$1" == "true" ] || [ "$2" == "true" ]
