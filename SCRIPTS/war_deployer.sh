b304a6e92b02a2ab3f56aa8d9ed4b4ba9843c027
Dinuka Jayamaha <dinukaj@zone24x7.com>
2017-02-07 12:22:29 +0530

    Update war_deployer.sh

--- SCRIPTS/war_deployer.sh
+++ SCRIPTS/war_deployer.sh
@@ -131,6 +131,7 @@ hermes_app_deploy()
     cd $HERMESE_APP_LOC/temp
     unzip *.zip
     rm -rf $HERMESE_APP_LOC/$BUILD_NAME/
+    chmod u+x $HERMESE_APP_LOC/temp/hermese.bsx
     $HERMESE_APP_LOC/temp/hermese.bsx
     cp -rf $HERMESE_APP_LOC/temp/SERVER_CONFIGS/*.xml $HERMESE_APP_LOC/$BUILD_NAME/.
     cp -rf $HERMESE_APP_LOC/temp/SERVER_CONFIGS/Hermese.sh $HERMESE_APP_LOC/$BUILD_NAME/.
